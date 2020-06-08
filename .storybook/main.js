f2b8fa6c7b033ff994a57c726cbd69ab24bdf736
Cory Perkins <link2cory@gmail.com>
2020-06-08 15:09:10 -0700

    Initial Commit, project generated with cookiecutter-jam-app

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../src/**/*.stories.[tjs]sx'],
+  addons: [
+    '@storybook/preset-typescript',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+}
